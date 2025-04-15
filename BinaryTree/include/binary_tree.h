421520dae9be456c908eaa77629666c7ea512e62
kant13 <yevka.vladyslav@gmail.com>
2025-04-15 17:08:10 +0300

    impl methods size, empty, add, find

--- BinaryTree/include/binary_tree.h
+++ BinaryTree/include/binary_tree.h
@@ -20,11 +20,16 @@ public:
     bool empty() const;
     void add(int val);
     void erase(int val);
-    int* find(int val) const;
+    bool find(int val) const;
 
 private:
     TreeNode* root_;
     size_t count_;
+
+private:
+    bool find(TreeNode* node, int val) const;
+    void add(TreeNode*& node, int val);
+    void erase(TreeNode*& node, int val);
 };
 
 inline size_t binary_tree::size() const {
@@ -32,17 +37,78 @@ inline size_t binary_tree::size() const {
 }
 
 inline bool binary_tree::empty() const {
-    return root_ == nullptr;
+    return (root_ == nullptr);
+}
+
+void binary_tree::add(TreeNode*& node, int val) {
+    if (!node) {
+        node = new TreeNode(val);
+        ++count_;
+        return;
+    }
+
+    if (node->val < val) {
+        add(node->left, val);
+    }
+    else if (node->val > val) {
+        add(node->right, val);
+    }
 }
 
 inline void binary_tree::add(int val) {
+    add(root_, val);
+}
 
+// FIXME
+inline void binary_tree::erase(TreeNode*& node, int val) {
+    if (!node) {
+        return;
+    }
+
+    if (node->val < val) {
+        erase(node->left, val);
+    }
+    else if (node->val > val) {
+        erase(node->right, val);
+    }
+    else {
+        if (node->left && node->right) {
+
+        }
+        else if (!node->left && node->right) {
+
+        }
+        else if (node->left && !node->right) {
+
+        }
+        else { // !node->left && !node->right
+            delete node;
+            node = nullptr;
+            --count_;
+        }
+    }
 }
 
 inline void binary_tree::erase(int val) {
-
+    erase(root_, val);
 }
 
-inline int* binary_tree::find(int val) const {
-    return nullptr;
+inline bool binary_tree::find(TreeNode* node, int val) const {
+    if (!node) {
+        return false;
+    }
+
+    if (node->val < val) {
+        find(node->left, val);
+    }
+    else if (node->val > val) {
+        find(node->right, val);
+    }
+    else {
+        return true;
+    }
+}
+
+inline bool binary_tree::find(int val) const {
+    return find(root_, val);
 }
--- BinaryTree/tests/test_binary_tree.cxx
+++ BinaryTree/tests/test_binary_tree.cxx
@@ -1,11 +1,11 @@
-#include "catch.hpp"
+﻿#include "catch.hpp"
 #include "binary_tree.h"
 
 #include <string>
 
 TEST_CASE("empty, first call", "binary_tree") {
     binary_tree bt;
-    REQUIRE(bt.empty());
+    REQUIRE(bt.empty() == true);
 }
 
 TEST_CASE("size, first call", "binary_tree") {
@@ -13,29 +13,165 @@ TEST_CASE("size, first call", "binary_tree") {
     REQUIRE(bt.size() == 0);
 }
 
-TEST_CASE("add", "binary_tree") {
+TEST_CASE("add, 1 node", "binary_tree") {
     binary_tree bt;
-    const int inVal = 42;
-    bt.add(inVal);
-    REQUIRE(!bt.empty());
+    bt.add(42);
+    REQUIRE(bt.empty() == false);
     REQUIRE(bt.size() == 1);
+}
+
+TEST_CASE("add, 2 identical nodes", "binary_tree") {
+    binary_tree bt;
+    bt.add(42);
+    bt.add(42);
+    REQUIRE(bt.empty() == false);
+    REQUIRE(bt.size() == 1);
+}
+
+TEST_CASE("add, 2 node", "binary_tree") {
+    binary_tree bt;
+    bt.add(1);
+    bt.add(2);
+    REQUIRE(bt.empty() == false);
+    REQUIRE(bt.size() == 2);
 }
 
 TEST_CASE("find, bt is empty", "binary_tree") {
     binary_tree bt;
     const int inVal = 42;
-    int* out = bt.find(inVal);
-    REQUIRE(out == nullptr);
+    REQUIRE(bt.find(inVal) == false);
 }
 
-TEST_CASE("find, bt is not empty", "binary_tree") {
+TEST_CASE("find, bt has 1 node", "binary_tree") {
     binary_tree bt;
     const int inVal = 42;
     bt.add(inVal);
-    REQUIRE(!bt.empty());
+    REQUIRE(bt.empty() == false);
     REQUIRE(bt.size() == 1);
+    REQUIRE(bt.find(inVal));
+}
 
-    int* out = bt.find(inVal);
-    REQUIRE(out != nullptr);
-    REQUIRE(*out == inVal);
+TEST_CASE("find, bt has 2 node: 1, 2", "binary_tree") {
+    binary_tree bt;
+    bt.add(1);
+    bt.add(2);
+
+    REQUIRE(bt.empty() == false);
+    REQUIRE(bt.size() == 2);
+
+    REQUIRE(bt.find(0) == false);
+    REQUIRE(bt.find(1));
+    REQUIRE(bt.find(2));
+}
+
+
+/*
+* Pre-order 10 → 5 → 2 → 7 → 15 → 12 → 20
+       10
+      /  \
+     5    15
+    /  \  /  \
+    2  7  12  20
+*/
+TEST_CASE("find, 10 5 2 7 15 12 30", "binary_tree") {
+    binary_tree bt;
+    bt.add(10);
+    bt.add(5);
+    bt.add(2);
+    bt.add(7);
+    bt.add(15);
+    bt.add(12);
+    bt.add(20);
+
+    REQUIRE(bt.empty() == false);
+    REQUIRE(bt.size() == 7);
+
+    REQUIRE(bt.find(2));
+    REQUIRE(bt.find(7));
+    REQUIRE(bt.find(12));
+    REQUIRE(bt.find(20));
+}
+
+/*
+* Pre-order 10 → 5 → 2 → 7 → 15 → 12 → 20
+       10
+      /  \
+     5    15
+    /  \  /  \
+    2  7  12  20
+*/
+TEST_CASE("erase last left-left node, 10 5 2 7 15 12 30", "binary_tree") {
+    binary_tree bt;
+    bt.add(10);
+    bt.add(5);
+    bt.add(2);
+    bt.add(7);
+    bt.add(15);
+    bt.add(12);
+    bt.add(20);
+
+    REQUIRE(bt.empty() == false);
+    REQUIRE(bt.size() == 7);
+
+    // last left-left node
+    REQUIRE(bt.find(2));
+    bt.erase(2);
+    REQUIRE(bt.size() == 6);
+    REQUIRE(bt.find(2) == false);
+
+}
+
+/*
+* Pre-order 10 → 5 → 2 → 7 → 15 → 12 → 20
+       10
+      /  \
+     5    15
+    /  \  /  \
+    2  7  12  20
+*/
+TEST_CASE("erase right-right node, 10 5 2 7 15 12 30", "binary_tree") {
+    binary_tree bt;
+    bt.add(10);
+    bt.add(5);
+    bt.add(2);
+    bt.add(7);
+    bt.add(15);
+    bt.add(12);
+    bt.add(20);
+
+    REQUIRE(bt.empty() == false);
+    REQUIRE(bt.size() == 7);
+
+    // last right-right node
+    REQUIRE(bt.find(20));
+    bt.erase(20);
+    REQUIRE(bt.size() == 6);
+    REQUIRE(bt.find(20) == false);
+}
+
+/*
+* Pre-order 10 → 5 → 2 → 7 → 15 → 12 → 20
+       10
+      /  \
+     5    15
+    /  \  /  \
+    2  7  12  20
+*/
+TEST_CASE("erase 5 node, 10 5 2 7 15 12 30", "binary_tree") {
+    binary_tree bt;
+    bt.add(10);
+    bt.add(5);
+    bt.add(2);
+    bt.add(7);
+    bt.add(15);
+    bt.add(12);
+    bt.add(20);
+
+    REQUIRE(bt.empty() == false);
+    REQUIRE(bt.size() == 7);
+
+    REQUIRE(bt.find(5));
+    bt.erase(5);
+    REQUIRE(bt.size() == 6);
+    REQUIRE(bt.find(5) == false);
 }
